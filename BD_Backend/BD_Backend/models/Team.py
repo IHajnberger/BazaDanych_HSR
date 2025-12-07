4a7f1f40059e2280ba2c1adabc9187bfcace8422
IHajnberger <280138@student.pwr.edu.pl>
2025-12-07 16:22:27 +0100

    Poprawki

--- BD_Backend/BD_Backend/models/Team.py
+++ BD_Backend/BD_Backend/models/Team.py
@@ -13,5 +13,33 @@ class Team(db.Model):
     Characters= db.relationship('Character', secondary=Team_Character, back_populates='Teams') # relacja wiele do wielu
     Users = db.relationship('User', secondary=User_Team, back_populates='Teams') # relacja wiele do wielu
     
+    # do_dict z automatycznym przypisaniem slotow
     def to_dict(self):
-        return {"id": self.id, "Name": self.Name, "Score": self.Score}
+        # generowanie slotow na podstawie Character.Role
+        slots = {1: None, 2: None, 3: None, 4: None}
+
+        # DPS - slot 1
+        for c in self.Characters:
+            if c.Role == "DPS":
+                slots[1] = c
+
+        # Support - slot 2 i 3
+        support_index = 2
+        for c in self.Characters:
+            if c.Role == "Support":
+                if support_index <= 3:
+                    slots[support_index] = c
+                    support_index += 1
+
+        # Sustain - slot 4
+        for c in self.Characters:
+            if c.Role == "Sustain":
+                slots[4] = c
+
+        return {"id": self.id, "Name": self.Name, "Score": self.Score,
+            "Characters": 
+            {
+                slot: char.Name if char else None
+                for slot, char in slots.items()
+            }
+        }
