557760bfd725118fc7fcdf254eec4c07b743275c
IHajnberger <280138@student.pwr.edu.pl>
2026-01-02 23:52:06 +0100

    walka z localhostem (przegrałam)
    Windows moment

--- BD_Backend/BD_Backend/runserver.py
+++ BD_Backend/BD_Backend/runserver.py
@@ -18,4 +18,9 @@ with app.app_context():
     run_seed()
 
 if __name__ == "__main__":
-    app.run(debug=True, use_reloader=False)
+    app.run(
+        host="127.0.0.1",
+        port=5000,
+        debug=True,
+        use_reloader=False
+    )
