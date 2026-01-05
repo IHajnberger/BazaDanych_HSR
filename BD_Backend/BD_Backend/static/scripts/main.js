9212744e02730449f7d1c21d804746990667b0af
IHajnberger <280138@student.pwr.edu.pl>
2026-01-05 19:31:09 +0100

    data overview pierwsza połowa
    od postaci

--- BD_Backend/BD_Backend/static/scripts/main.js
+++ BD_Backend/BD_Backend/static/scripts/main.js
@@ -5,6 +5,7 @@ if (!userId) {
     window.location.href = "/";
 }
 
+// username load
 async function loadUser() {
     try {
         const response = await fetch(`/api/users/${userId}`);
@@ -21,6 +22,29 @@ function logout() {
     window.location.href = "/";
 }
 
+// overview load
+async function loadOverview() {
+    const userId = localStorage.getItem("user_id");
+    if (!userId) return;
+
+    try {
+        const [charsRes, teamsRes] = await Promise.all([
+            fetch(`/api/users/${userId}/characters`),
+            fetch(`/api/users/${userId}/teams`)
+        ]);
+
+        const characters = await charsRes.json();
+        const teams = await teamsRes.json();
+
+        document.getElementById("charactersCount").textContent = characters.length;
+        document.getElementById("teamsCount").textContent = teams.length;
+
+    } catch (err) {
+        console.error("Failed to load overview");
+    }
+}
+
+// load owned characters
 async function loadMyCharacters() {
     const userId = localStorage.getItem("user_id");
     if (!userId) return;
@@ -51,3 +75,4 @@ async function loadMyCharacters() {
 
 loadMyCharacters();
 loadUser();
+loadOverview();
